7ae22ec4d6fe4310de5b531bd238cca819ff40f7
olucascdev <lucasdevproject@gmail.com>
2025-09-27 03:38:14 -0300

    chore(factories): create VoteFactory

---
+++ database/factories/VoteFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<\App\Models\Vote>
+ */
+final class VoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
