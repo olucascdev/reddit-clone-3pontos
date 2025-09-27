d7c24f65655be820fb887dbdacb84b9976f9d18a
olucascdev <lucasdevproject@gmail.com>
2025-09-27 03:37:00 -0300

    chore(factories): create PostFactory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<\App\Models\Post>
+ */
+final class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
