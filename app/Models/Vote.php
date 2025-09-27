11ebc14c6f0a6319233b207433a22a80d7cd3100
olucascdev <lucasdevproject@gmail.com>
2025-09-27 03:34:06 -0300

    feat(models): add Vote model

---
+++ app/Models/Vote.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+final class Vote extends Model
+{
+    /** @use HasFactory<\Database\Factories\VoteFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'value',
+    ];
+
+    /**
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class)->withDefault([
+            'name' => '[deleted]',
+            'avatar' => null,
+        ]);
+    }
+
+    public function votable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
