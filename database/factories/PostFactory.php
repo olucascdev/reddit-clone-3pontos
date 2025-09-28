df3357782a0eb02b4eca0edc28c6d0ba343643e0
olucascdev <lucasdevproject@gmail.com>
2025-09-27 22:35:13 -0300

    feat(factory): implement Post factory for database seeding

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
- * @extends Factory<\App\Models\Post>
+ * @extends Factory<Post>
  */
 final class PostFactory extends Factory
 {
@@ -18,8 +20,13 @@ final class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->sentence();
+
         return [
-            //
+            'title' => $title,
+            'slug' => Str::slug(Str::limit($title, 40, '')),
+            'photo' => fake()->imageUrl(),
+            'content' => fake()->paragraphs(3, true),
         ];
     }
 }
