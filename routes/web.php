f29622cb07300bef576c7e0b14e0d1ae24517bb3
olucascdev <lucasdevproject@gmail.com>
2025-09-29 01:46:56 -0300

    chore(route): remove deprecated route dashboard

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn (): View|Factory => view('welcome'));
-
-Route::get('/dashboard', fn (): View|Factory => view('dashboard'))->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', fn (): View|Factory => view('welcome'))->name('home');
 
 Route::middleware('auth')->group(function (): void {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
