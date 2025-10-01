cd4a8b4832bb9290bd480144f49e3165460933dc
olucascdev <lucasdevproject@gmail.com>
2025-10-01 02:35:25 -0300

    feat(layout): create app.blade.php for main layout and update guest.blade.php structure

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -1,23 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+?>
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-50">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>Reddit-like Home • Laravel</title>
+        <title>Reddit-like Home • Laravel</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net" />
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
 
-    <!-- Styles / Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class="min-h-full ">
-<!-- Top Nav -->
-    <x-navbar />
-
-    <!-- Content -->
-    {{ $slot }}
-</body>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="bg-elevation-surface min-h-full">
+        <x-navbar />
+        {{ $slot }}
+    </body>
 </html>
+<?php 
